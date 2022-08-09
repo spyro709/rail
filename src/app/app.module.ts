f3a9450136d733b4c15928f7dc8d29779ef1a84d
spyro709 <spyrothedragon700@gmail.com>
2022-08-09 16:18:33 +0200

    hello wrold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SelectComponent } from './select/select.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectComponent
   ],
   imports: [
     BrowserModule
