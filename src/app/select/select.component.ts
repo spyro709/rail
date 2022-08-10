b53ec39a6e6938c6af3f7d120a1e7bd3999c5e30
spyro709 <spyrothedragon700@gmail.com>
2022-08-10 09:38:33 +0200

    some styling stuff

--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
-  styleUrls: ['./select.component.scss']
+  styleUrls: ['./select.component.scss'],
 })
 export class SelectComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  el: any;
+  constructor() {
+    this.el = document.querySelector('.book');
   }
 
+  ngOnInit(): void {}
+
+  hoverHandler() {
+    document.querySelector('.book')?.classList.add('small');
+    document.querySelector('.movie')?.classList.add('in');
+  }
+  leaveHanlder() {
+    document.querySelector('.book')?.classList.remove('small');
+    document.querySelector('.movie')?.classList.remove('in');
+  }
 }
